806f267534a54fb4210b9dd293eb26ddee2cfd29
yashkurkure <yash.kurkure1903@gmail.com>
2022-02-11 16:21:21 -0600

    "New tests for set operations"

--- src/test/scala/TestSetOperations.scala
+++ src/test/scala/TestSetOperations.scala
@@ -1,8 +1,8 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import collection.mutable.Set
 import SetLangDSL.SetLang.construct.*
 import SetLangDSL.SetLang.setOperation.*
+import scala.collection.mutable
 
 /**
  * This test suite tests the set operations of setLang
@@ -25,7 +25,7 @@ class TestSetOperations extends AnyFlatSpec with Matchers{
 
     //Create a set and bind it to a name
     Assign(Variable("testSet"), Insert(1,2,3,4,5,6,7,8,9)).eval()
-    
+
     //Delete the value 1 from the set
     Assign(Variable("testSet"), Delete(Value(1))).eval()
 
@@ -36,6 +36,14 @@ class TestSetOperations extends AnyFlatSpec with Matchers{
   }
 
   it should "Delete a value from a set. [Case: Value is not present in set]" in{
+    //Create a set and bind it to a name
+    Assign(Variable("testSet"), Insert(1,2,3,4,5,6,7,8,9)).eval()
+
+    //Delete the value 11 from the set
+    Assign(Variable("testSet"), Delete(Value(11))).eval()
+
+    val actualValue = Variable("testSet").eval().getValue()
+    val expectedValue = Set(1,2,3,4,5,6,7,8,9)
 
   }
 
@@ -52,15 +60,44 @@ class TestSetOperations extends AnyFlatSpec with Matchers{
   }
 
   it should "return the intersection of two sets" in{
+    //first let us create a set and assign it to a variable
+    val set1 = Insert(1,2,3,4,5,11).eval() //no need to call getValue(), since Union accepts (Value(Set), Value(Set))
+    val set2 = Insert(6,7,8,9,10,11).eval()
+
+    val actualValue = Intersection(set1, set2).eval().getValue()
+    val expectedValue = Set(11)
+
+    //compare the values
+    compareValues(actualValue, expectedValue) shouldBe true
 
   }
 
 
   it should "return the difference of two sets" in{
 
+    //first let us create a set and assign it to a variable
+    val set1 = Insert(1,2,3,4,5).eval() //no need to call getValue(), since Union accepts (Value(Set), Value(Set))
+    val set2 = Insert(4,5).eval()
+
+    val actualValue = Difference(set1, set2).eval().getValue()
+    val expectedValue = Set(1,2,3)
+
+    //compare the values
+    compareValues(actualValue, expectedValue) shouldBe true
+
   }
 
   it should "return the symmetric difference of two sets" in{
+
+    //first let us create a set and assign it to a variable
+    val set1 = Insert(1,2,3,4,5).eval() //no need to call getValue(), since Union accepts (Value(Set), Value(Set))
+    val set2 = Insert(2,4,6).eval()
+
+    val actualValue = SymmetricDifference(set1, set2).eval().getValue()
+    val expectedValue = Set(1,3,5,6)
+
+    //compare the values
+    compareValues(actualValue, expectedValue) shouldBe true
 
   }
 
