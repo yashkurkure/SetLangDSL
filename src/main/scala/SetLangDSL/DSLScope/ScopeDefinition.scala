1e6d48185402df68f8753404eba45ec38f77d08e
yashkurkure <yash.kurkure1903@gmail.com>
2022-03-24 10:43:38 -0500

    Updated Comments

--- src/main/scala/SetLangDSL/DSLScope/ScopeDefinition.scala
+++ src/main/scala/SetLangDSL/DSLScope/ScopeDefinition.scala
@@ -1,19 +1,60 @@
+/*
+ *
+ *  Copyright (c) 2022. Yash Kurkure. All rights reserved.
+ *
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
 package SetLangDSL.DSLScope
 
+// Scala Imports
+import scala.annotation.targetName
+
+// DSL Imports
 import SetLangDSL.DSL.*
 import SetLangDSL.DSLClass.ClassDefinition
 
-import scala.annotation.targetName
 
+
+/**
+ * class ScopeDefinition
+ *
+ * This class represents a scope
+ *
+ * Constructors
+ * ScopeDefinition(parent: ScopeDefinition)
+ *    parent: Is the parent scope for the newly defined scope
+ *
+ * */
 class ScopeDefinition(parent: ScopeDefinition) {
+
 
   // Book keeping for the scope's bindings
   val bindings: ScopeBindings = new ScopeBindings(this)
 
+
+  /**
+   * Assign
+   *
+   * Returns the bindings, on which you can
+   *  call operations to create new bindings
+   * */
   def Assign: ScopeBindings = {
     bindings
   }
 
+
+  /**
+   * Variable
+   *  parameters:
+   *    name: Variable name (binding name)
+   *
+   * Used to access an existing binding.
+   * The value of the binding is returned using Value()
+   * If the binding does not exist, it would return null
+   * */
   def Variable(name: String): Value= {
     // Get the incomplete binding (This can tell us if the binding exists or not)
     val incompleteBinding = bindings.Variable(name)
@@ -33,11 +74,32 @@ class ScopeDefinition(parent: ScopeDefinition) {
       null
   }
 
+
+  /**
+   * ExecuteMacro
+   *  parameters:
+   *    macroName: Name of the macro to be executed
+   *    variable: Variable on which the macro is to be executed
+   *
+   * Used to execute a macro
+   * Example:
+   *  s.ExecuteMacro("myMacro", s.Variable("x"))
+   * */
   def ExecuteMacro(macroName: String, variable: Value): Unit = {
     val macroBody = this.Variable(macroName).getValue.asInstanceOf[Value=>Unit]
     macroBody(variable)
   }
 
+
+  /**
+   * Scope
+   *  parameters:
+   *    f: A function that takes in the scope definition
+   *
+   * Used to create a child anonymous scope
+   * An anonymous scope is not stored in the bindings, thus
+   *  once it is executed, all its data would disappear
+   * */
   @targetName("Create Anonymous Scope")
   def Scope(f:ScopeDefinition=>Unit): Unit = {
     //println("Creating Anonymous Scope")
@@ -47,6 +109,17 @@ class ScopeDefinition(parent: ScopeDefinition) {
     f(scope)
   }
 
+
+  /**
+   * Scope
+   *  parameters:
+   *    scopeName: string representing the name of the scope
+   *    f: A function that takes in the scope definition
+   *
+   * Used to create a child named scope
+   * Named scope is stored in the bindings, thus
+   *  once it is executed, it's data can be accessed later
+   * */
   @targetName("Create Named Scope")
   def Scope(scopeName: String, f:ScopeDefinition => Unit): Unit = {
     //println("Creating Named Scope")
@@ -60,6 +133,16 @@ class ScopeDefinition(parent: ScopeDefinition) {
     this.Assign.Variable(scopeName).toValue(scope)
   }
 
+
+  /**
+   * Scope
+   *  parameters:
+   *    scopeName: string representing the name of a scope
+   *
+   * Used to access a named scope
+   * One can access all the bindings of the named scope by using
+   *  this as an entry point.
+   * */
   @targetName("Get Named Scope")
   def Scope(scopeName: String): ScopeDefinition = {
     //search if the binding exists
@@ -84,6 +167,7 @@ class ScopeDefinition(parent: ScopeDefinition) {
     //return null as the binding was not found
       null
   }
+
 
   /**
    * ClassDef()
